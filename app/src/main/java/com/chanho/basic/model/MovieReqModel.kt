3721216d5e354cad494c963bb762d76aab366491
chan33344 <chan33344@gmail.com>
2021-02-04 23:58:25 +0900

    네이버 api 영화검색 요청모델 및 헤더 추가

--- app/src/main/java/com/chanho/basic/model/MovieReqModel.kt
+++ app/src/main/java/com/chanho/basic/model/MovieReqModel.kt
@@ -2,11 +2,11 @@ package com.chanho.basic.model
 
 data class MovieReqModel(
     // 원하는 검색어 필수
-    val query:String,
+    var query:String,
     // 검색 결과 출력 건수 지정 최대 100 개 가능
-    val display:String?,
+    var display:String,
     // 검색의 시작 위치 지정 가능 초히대 1000까지 가능
-    val start:String?,
+    var start:String,
     /**
      * 검색을 원하는 장르 코드를 의미한다. 영화 코드는 다음과 같다.
     1: 드라마 2: 판타지
@@ -24,25 +24,25 @@ data class MovieReqModel(
     25: 실험 26: 영화카툰
     27: 영화음악 28: 영화패러디포스터
      */
-    val genre:String?,
+    var genre:String?,
     /**
      * 검색을 원하는 국가 코드를 의미한다. 국가코드는 대문자만 사용 가능하며,
     분류는 다음과 같다.
     한국 (KR), 일본 (JP), 미국 (US), 홍콩 (HK),
     영국 (GB), 프랑스 (FR), 기타 (ETC)
      */
-    val country:String?,
+    var country:String?,
     /**
      * 검색을 원하는 영화의 제작년도(최소)를 의미한다.
     yearfrom은 yearto와 함께 사용되어야 한다.
      */
-    val yearfrom:String?,
+    var yearfrom:String?,
     /**
      * 검색을 원하는 영화의 제작년도(최대)를 의미한다.
     yearto는 yearfrom과 함께 사용되어야 한다.
      */
-    val yearto:String?
+    var yearto:String?
 
 ){
-    constructor():this("가을","10","1","","","","")
+    constructor():this("사랑","10","1","","","","")
 }
